1738a2e953287cf5762d8ce69dbcabb87985e337
Harpica <harpy.ka@gmail.com>
2023-02-08 14:30:11 +0300

    refactor: released PopupForm component

---
+++ src/components/PopupForm.jsx
@@ -0,0 +1,34 @@
+const PopupForm = ({name, onClose, title, onSubmit, isLoading, isValid, buttonText, buttonLoadingText, children}) => {
+  function handleSubmit(e) {
+    e.preventDefault();
+    onSubmit(); }
+  return (
+    <form
+    className={`form ${name}__form`}
+    name={`${name}-form`}
+    onSubmit={handleSubmit}
+  >
+    <button
+      className='button close-button'
+      type='button'
+      onClick={onClose}
+    ></button>
+    <h2 className='popup__title'>{title}</h2>
+    <fieldset className='form__input-container'>
+      {children}
+      <button
+        type='submit'
+        className={`form__button ${
+          isValid ? '' : 'form__button_disabled'
+        }`}
+        aria-label='Сохранить изменения'
+        disabled={isValid? false: true}
+      >
+        {isLoading ? buttonLoadingText : buttonText}
+      </button>
+    </fieldset>
+  </form>
+   );
+}
+
+export default PopupForm;
--- src/components/PopupWithForm.js
+++ src/components/PopupWithForm.js
@@ -1,54 +1,14 @@
 import React from 'react';
+import PopupForm from './PopupForm';
 
-const PopupWithForm = React.memo(
-  ({
-    isOpen,
-    onClose,
-    onSubmit,
-    isLoading,
-    name,
-    title,
-    buttonText,
-    buttonLoadingText,
-    isValid,
-    popupRef,
-    children,
-  }) => {
-    function handleSubmit(e) {
-      e.preventDefault();
-      onSubmit();
-    }
-    return (
-      <section className={`popup ${name} ${isOpen ? 'popup_opened' : ''}`}>
-        <div className='popup__container' ref={popupRef}>
-          <form
-            className={`popup__form ${name}__form`}
-            name={`${name}-form`}
-            onSubmit={handleSubmit}
-          >
-            <button
-              className='button close-button'
-              type='button'
-              onClick={onClose}
-            ></button>
-            <h2 className='popup__title'>{title}</h2>
-            <fieldset className='popup__input-container'>
-              {children}
-              <button
-                type='submit'
-                className={`popup__button ${
-                  isValid ? '' : 'popup__button_disabled'
-                }`}
-                aria-label='Сохранить изменения'
-              >
-                {isLoading ? buttonLoadingText : buttonText}
-              </button>
-            </fieldset>
-          </form>
-        </div>
-      </section>
-    );
-  }
-);
+const PopupWithForm = React.memo(({ isOpen, popupRef, name, ...props }) => {
+  return (
+    <section className={`popup ${name} ${isOpen ? 'popup_opened' : ''}`}>
+      <div className='popup__container' ref={popupRef}>
+        <PopupForm name={name} {...props} />
+      </div>
+    </section>
+  );
+});
 
 export default PopupWithForm;
