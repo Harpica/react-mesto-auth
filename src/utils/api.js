506835304586f6e742cd093f4149345fcfd02b75
Harpica <harpy.ka@gmail.com>
2023-02-08 21:15:24 +0300

    feat: new api for userData fetching

--- src/utils/api.js
+++ src/utils/api.js
@@ -12,6 +12,41 @@ class Api {
     }
     return Promise.reject(`Ошибка: ${res.status}`);
   }
+}
+
+class AuthApi extends Api {
+  constructor({ baseUrl, headers }) {
+    super({ baseUrl, headers });
+  }
+  registerUser({ email, password }) {
+    return this._request(`${this.baseUrl}/signup`, {
+      method: 'POST',
+      headers: this.headers,
+      body: JSON.stringify({ email: email, password: password }),
+    });
+  }
+  loginUser({ email, password }) {
+    return this._request(`${this.baseUrl}/signin`, {
+      method: 'POST',
+      headers: this.headers,
+      body: JSON.stringify({ email: email, password: password }),
+    });
+  }
+  getUserData(userToken) {
+    this.headers['Authorization'] = `Bearer ${userToken}`;
+    const request = this._request(`${this.baseUrl}/users/me`, {
+      method: 'GET',
+      headers: this.headers,
+    });
+    delete this.headers['Authorization'];
+    return request;
+  }
+}
+
+class CardsApi extends Api {
+  constructor({ baseUrl, headers }) {
+    super({ baseUrl, headers });
+  }
   postCard({ name, link }) {
     return this._request(`${this.baseUrl}/cards`, {
       method: 'POST',
@@ -61,11 +96,18 @@ class Api {
     return isLiked ? this.removeLikeCard(cardID) : this.likeCard(cardID);
   }
 }
-// Создадим объект для общения с сервером
-export const api = new Api({
+// Создадим объекты для общения с сервером
+export const cardsApi = new CardsApi({
   baseUrl: 'https://mesto.nomoreparties.co/v1/cohort-56',
   headers: {
     authorization: '28c8be99-8fcb-4f8e-afaa-96e6bec99f34',
+    'Content-Type': 'application/json',
+  },
+});
+
+export const authApi = new AuthApi({
+  baseUrl: 'https://auth.nomoreparties.co',
+  headers: {
     'Content-Type': 'application/json',
   },
 });
