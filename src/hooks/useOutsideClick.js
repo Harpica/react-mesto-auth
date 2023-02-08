87b3400d054108a0c731ef598de251889717bd62
Harpica <harpy.ka@gmail.com>
2023-02-08 21:10:33 +0300

    fix: re-renders and outsideClick function

--- src/hooks/useOutsideClick.js
+++ src/hooks/useOutsideClick.js
@@ -6,15 +6,15 @@ export default function useOutsideClick(handleClose, isOpen) {
   const ref = React.useRef(null);
   React.useEffect(() => {
     const handleOutsideClick = (event) => {
-      if (ref?.current?.contains && !ref.current.contains(event.target)) {
+      if (!ref.current.contains(event.target)) {
         handleClose();
+        document.removeEventListener(MOUSE_UP, handleOutsideClick, false);
       }
     };
     if (isOpen) {
       document.addEventListener(MOUSE_UP, handleOutsideClick, false);
       return;
     }
-    document.removeEventListener(MOUSE_UP, handleOutsideClick, false);
   }, [handleClose, isOpen]);
   return ref;
 }
